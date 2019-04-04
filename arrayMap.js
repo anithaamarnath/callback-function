2065ebe458752901b51ee9c04863dfccdd8b6f86
anithaamarnath <anithaamarnath04@gmail.com>
2019-04-04 21:54:42 +0000

    Array Mapping

---
+++ arrayMap.js
@@ -0,0 +1,12 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function (sqr){
+  return Math.sqrt(sqr.x*sqr.x + sqr.y * sqr.y)
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
